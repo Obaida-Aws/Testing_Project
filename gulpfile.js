a9984b45a750fdbcc9e0e54148f3cb56d013c93a
Obaida-Aws <s11923787@stu.najah.edu>
2025-05-19 17:15:13 +0300

    final edit

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 import gulp from 'gulp';
 import gulpShell from 'gulp-shell';
 
-// مهمة بناء وتشغيل Parcel
 gulp.task('default', gulpShell.task('parcel ./starter/index.html --open'));
 
-// مهمة لتشغيل الاختبارات
+
 gulp.task('test', gulpShell.task('npm run test'));
